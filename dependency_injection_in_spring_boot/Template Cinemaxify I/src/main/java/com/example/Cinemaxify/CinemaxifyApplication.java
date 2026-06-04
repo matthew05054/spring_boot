808b20d47fad31294c0f81db2bae3a574a4876dd
MATTHEW <matthewmansah@gmail.com>
2026-06-05 00:08:50 +0530

    dependenc injection

--- dependency_injection_in_spring_boot/Template Cinemaxify I/src/main/java/com/example/Cinemaxify/CinemaxifyApplication.java
+++ dependency_injection_in_spring_boot/Template Cinemaxify I/src/main/java/com/example/Cinemaxify/CinemaxifyApplication.java
@@ -21,24 +21,46 @@ public class CinemaxifyApplication {
 		switch (userChoice) {
 		  case 1 -> {
 			  // a. Set userType according to the userChoice.
+			  userType = "self";
 		  }
 		  case 2 -> {
 			  // b. Set userType according to the userChoice.
+			  userType = "spouse";
 		  }
 		  case 3 -> {
 			  // c. Print "Exiting..." and return
+			  System.out.println("Existing..");
+			  return;
 				    }
 		  default -> {
 			  // d. Print "Invalid choice." and return.
-
+			  System.out.println("Invalid choice");
+			  return;
 				     }
 		}
 		// e. Pick the user bean using context.getBean().
-			User user = ;
+			User user = (User) context.getBean(userType);
 		// f. Take input for User details i.e. name , age, address etc.
+		System.out.println("Enter Name..!");
+		String name = scanner.next();
+
+		System.out.println("Enter age..!");
+		int age = scanner.nextInt();
+
+		System.out.println("Enter contact..!");
+		Long contact = scanner.nextLong();
+
+		System.out.println("Enter address..!");
+		String address = scanner.next();
+
+		
 
 		// g. Set the above fetched details into the user by using appropriate method.
 
+		user.setUserDetails(name, age, contact, address);
+
 		// h. finally print the details by using appropriate method.
+
+		user.getUserDetails();
 			}
 	}
--- dependency_injection_in_spring_boot/Template Cinemaxify I/src/main/java/com/example/Cinemaxify/Spouse.java
+++ dependency_injection_in_spring_boot/Template Cinemaxify I/src/main/java/com/example/Cinemaxify/Spouse.java
@@ -1,11 +1,28 @@
 package com.example.Cinemaxify;
 
 // Override the User Interface methods in Spouse class
-public class Spouse{
+public class Spouse implements User{
     private String name;
-    private String memberName = "spouse";
+    // private String memberName = "spouse";
     private int age;
     private Long contact;
     private String address;
+    @Override
+    public void setUserDetails(String name, int age, Long contact, String address) {
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+        this.address = address;
+
+    }
+    @Override
+    public void getUserDetails() {
+
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(contact);
+            System.out.println(address);
+        
+    }
 
 }
